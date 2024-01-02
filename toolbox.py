38f0e83d8b159d4cbbd131ebb542428664f517d7
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-02 11:46:46 +0100

    added nmap folder

--- toolbox.py
+++ toolbox.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+from nmap.nmap import nmap
 
+print("Welcome to ToolBox! \nPlease choose a option. \n1: nmap \n2: hydra\n3: burpsuite")
 
-print("hello nils")
+userInput = input("use: ")
+
+if userInput == "1":
+    nmap()
+elif userInput == "2":
+    print("hydra")
+elif userInput == "3":
+    print("burpsuite")
+else: 
+    print("please enter a valid option")
