0c38d2419aa65894be3fe2d6c392d943f40bc11f
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-02 13:16:24 +0100

    tetsing git

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -8,6 +8,6 @@ def nmap(ip):
     print("Nmap scan completed")
 
 if __name__ == "__main__":
-    target_ip = input("Enter the target IP address or range: ")
+    target_ip = input("enter the target IP address or range: ")
     nmap(target_ip)
 
