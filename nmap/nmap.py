256ae34932e7c144e98268eaf5581ab9e0bf37d7
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-05 18:56:53 +0100

    test

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -3,7 +3,7 @@
 import subprocess
 
 def nmap(ip):
-    command = f"nmap -sV -oN ../scan_results/scan_result {ip}"
+    command = f"nmap -sV -oN ../scan_results {ip}"
     subprocess.run(command, shell=True)
     print("Nmap scan completed, scan saved to directory scan_results, inside python_toolbox directory ")
 
