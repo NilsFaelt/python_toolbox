55734d8d4c37c122bfd0dec1c18eec30275c6589
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-05 19:18:03 +0100

    test

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -3,9 +3,9 @@
 import subprocess
 
 def nmap(ip):
-    command = f"nmap -sV >> /home/scan_result {ip}"
+    command = f"nmap -sV -p- -oN /home/scan_result {ip}"
     subprocess.run(command, shell=True)
-    print("Nmap scan completed, scan saved to home as scan_result")
+    print("Nmap scan completed, scan report saved to home as scan_result")
 
 if __name__ == "__main__":
     target_ip = input("enter the target IP address or range: ")
