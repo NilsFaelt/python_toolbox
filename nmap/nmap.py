50795179d941b132af7172f2cff54320d9014cd5
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-05 18:44:07 +0100

    test

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -2,10 +2,11 @@
 
 import subprocess
 
-def nmap(ip):
-    command = f"nmap -oN ~/Desktop/scan_result {ip}"
-    subprocess.run(command, shell=True)
-    print("Nmap scan completed")
+def nmap(ip): 
+    home_directory = os.path.expanduser("~")
+    scan_result_path = os.path.join(home_directory, "scan_result.txt")
+    command = f"nmap -oN {scan_result_path} {ip}"
+    os.system(command)
 
 if __name__ == "__main__":
     target_ip = input("enter the target IP address or range: ")
