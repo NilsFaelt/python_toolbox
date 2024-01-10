9a9aba577a523f50bb8c705e0e6ee3bad2bf1a9a
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-10 10:27:39 +0100

    testing os

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
+import os
 import config
 import subprocess
 
-
 def nmap(ip):
-    command = f"nmap -sV -p- >> {config.path_saved_scan_result}scan_result_nmap.txt {ip}"
+    scan_result_path = os.path.expanduser(f"{config.path_saved_scan_result}/scan_results/")  # Define the directory path
+    os.makedirs(scan_result_path, exist_ok=True)  # Create the directory if it doesn't exist
+
+    command = f"nmap -sV -p- >> {scan_result_path}scan_result_nmap.txt {ip}"
     subprocess.run(command, shell=True)
-    print(f"Nmap scan completed, scan report saved to {config.dir_saved_scan_result} as scan_result")
+    print(f"Nmap scan completed, scan report saved to {scan_result_path} as scan_result")
 
 if __name__ == "__main__":
     target_ip = input("enter the target IP address : ")
     nmap(target_ip)
-
