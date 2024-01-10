cb095b657f8bc43466b96113650acd7965618527
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-10 10:37:26 +0100

    test

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -8,7 +8,7 @@ def nmap(ip):
         scan_result_path = os.path.expanduser(f"{config.path_saved_scan_result}/scan_results/")
         os.makedirs(scan_result_path, exist_ok=True)
 
-        command = f"nmap -sV -p- >> {scan_result_path}scan_result_nmap.txt {ip}"
+        command = f"nmap -sV -p- >> {scan_result_path}test_scan_result_nmap.txt {ip}"
         subprocess.run(command, shell=True)
         print(f"Nmap scan completed, scan report saved to {scan_result_path} as scan_result")
     except Exception as e:
