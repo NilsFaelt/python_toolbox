9a57d717f6861d885fe6e5b45073e130c42bbab1
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-10 10:10:49 +0100

    update f-string in nmap

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -6,7 +6,7 @@ import subprocess
 def nmap(ip):
     command = f"nmap -sV -p- -oN {config.path_saved_scan_result}scan_result_nmap.txt {ip}"
     subprocess.run(command, shell=True)
-    print("Nmap scan completed, scan report saved to home as scan_result")
+    print(f"Nmap scan completed, scan report saved to {config.dir_saved_scan_result} as scan_result")
 
 if __name__ == "__main__":
     target_ip = input("enter the target IP address : ")
