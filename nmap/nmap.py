d75e3e1bbd81df13a1e072185e62b5262bac57d3
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-16 19:51:46 +0100

    fixed excetion

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -1,16 +1,21 @@
-import config
+
+"""Nmap"""
 import subprocess
-from util_functions.create_scan_results_path_and_dir import create_scan_results_path_and_dir
 import logging
+from util_functions.create_scan_results_path_and_dir import create_scan_results_path_and_dir
 
 def nmap(ip):
+    """Nmap preset scan for starting a scan"""
     try:
         scan_result_path = create_scan_results_path_and_dir()
         command = f"nmap -sV -p- >> {scan_result_path}scan_result_nmap.txt {ip}"
-        subprocess.run(command, shell=True)
+        subprocess.run(command, shell=True, check=True)
         print(f"Nmap scan completed, scan report saved to {scan_result_path} as scan_result")
-    except Exception as e:
-        logging.error(f"An error occurred during Nmap scan: {e}")
+    except subprocess.CalledProcessError as e:
+        logging.error("Error occurred during command execution.")
+        logging.error("Command: %s", e.cmd)
+        logging.error("Return Code: %s", e.returncode)
+        logging.error("Output: %s", e.output.decode("utf-8"))
 
 if __name__ == "__main__":
     target_ip = input("Enter the target IP address: ")
