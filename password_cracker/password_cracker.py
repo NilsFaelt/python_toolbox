27b5351f76014f42a77f29dcdde0f5ad3828b63f
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-18 13:53:57 +0100

    roll back

--- password_cracker/password_cracker.py
+++ password_cracker/password_cracker.py
@@ -5,22 +5,17 @@ hashed_words_array = []
 words_array = []
 
 
-def read_words_from_file():
-    with open(file_path, 'r', encoding='latin-1') as f:
-        for line in f:
-            words = line.strip().split()
-            hashed_words = [hashlib.sha256(word.encode('utf-8')).hexdigest() for word in words]
-            words_array.extend(words)
-            hashed_words_array.extend(hashed_words)
-
-
 def password_cracker(encoded_password):
     try:
-        read_words_from_file()
+        with open(file_path, 'r', encoding='latin-1', errors='replace') as f:
+            for line in f:
+                words = line.strip().split()
+                hashed_words = [hashlib.sha256(word.encode('utf-8')).hexdigest() for word in words]
+                words_array.extend(words)
+                hashed_words_array.extend(hashed_words)
         compare_hashes(encoded_password, hashed_words_array)
-    except UnicodeDecodeError as e:
-        print(f"Error: Unable to decode the file content. {e}")
-
+    except UnicodeDecodeError:
+        print("Error: Unable to decode the file content.")
 
 def compare_hashes(encoded_password, hashed_words_array):
     for index, hashed_password in enumerate(hashed_words_array):
@@ -29,4 +24,5 @@ def compare_hashes(encoded_password, hashed_words_array):
             print(f"Original word: {words_array[index]}")
             return
     print("No match found")
+
 
