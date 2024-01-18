5bef8e82873be5d6b07f4b19b0c6245a9e408531
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-18 12:59:52 +0100

    test

--- password_cracker/password_cracker.py
+++ password_cracker/password_cracker.py
@@ -6,12 +6,12 @@ passwords = []
 rock_you = []
 file_path = '/usr/share/wordlists/rockyou.txt'
 
-with open('hashed_passwords.txt', 'r') as f:
-    passwords = f.read().splitlines()
-with open(file_path, 'r') as f:
-    rock_you = f.read().splitlines()
 
 def password_cracker():
+    with open('hashed_passwords.txt', 'r') as f:
+        passwords = f.read().splitlines()
+    with open(file_path, 'r') as f:
+        rock_you = f.read().splitlines()
     """Function for cracking passwords"""
     
     print(rock_you)
