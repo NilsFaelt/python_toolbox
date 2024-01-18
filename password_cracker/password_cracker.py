e8af8b61f61042b5b34917ba2e0b7b443be2d5e1
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-18 13:10:53 +0100

    test

--- password_cracker/password_cracker.py
+++ password_cracker/password_cracker.py
@@ -1,23 +1,17 @@
-"""Password cracker"""
 import hashlib
-import os
 
-passwords = []
-rock_you = []
-file_path = '/usr/share/wordlists/rockyou.txt'
+file_path = '/usr/share/hashed_passwords.txt'
 
+hashed_words_array = []
 
-def password_cracker():
-    try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            for line in f:
-                words = line.strip().split()
-                rock_you.extend(words)
-    except UnicodeDecodeError:
-        print("Error: Unable to decode the file content.")
+try:
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            words = line.strip().split()
+            hashed_words = [hashlib.sha256(word.encode('utf-8')).hexdigest() for word in words]
+            hashed_words_array.extend(hashed_words)
+except UnicodeDecodeError:
+    print("Error: Unable to decode the file content.")
 
-
-    print(rock_you[0])
-
-
-    
+print(hashed_words_array[0])
+print(hashed_words_array[10])
