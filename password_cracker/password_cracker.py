9038806d30fd67958f5d776e11c61b926f94213a
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-18 13:03:47 +0100

    test

--- password_cracker/password_cracker.py
+++ password_cracker/password_cracker.py
@@ -8,17 +8,17 @@ file_path = '/usr/share/wordlists/rockyou.txt'
 
 
 def password_cracker():
+    """Function for cracking passwords"""
     with open('hashed_passwords.txt', 'r') as f:
         passwords = f.read().splitlines()
-    with open(file_path, 'r') as f:
-        rock_you = f.read().splitlines()
-    """Function for cracking passwords"""
+    with open(file_path, 'rb') as f:
+        rock_you = f.read()
     
     print(rock_you)
     print('passssssswoooooord crack')
 
-def generate_hash(password):
+# def generate_hash(password):
     
-    hash = hashlib.sha256(password.encode('utf-8'))
-    print(hash.hexdigest())
-    return hash    
+#     hash = hashlib.sha256(password.encode('utf-8'))
+#     print(hash.hexdigest())
+#     return hash    
