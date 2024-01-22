60eb1987989f7184b86cc0825c79cd52352b82fa
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-22 17:45:31 +0100

    test

--- setup.sh
+++ setup.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-python3 <<'EOF'
-import subprocess
-
-def update_packages():
-    """Updating packages"""
-    try:
-        subprocess.run(["apt-get", "update"], check=True)
-        subprocess.run(["apt-get", "upgrade", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "nmap", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "sqlmap", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "metasploit-framework", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "wireshark", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "john", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "hydra", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "aircrack-ng", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "maltego", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "nikto", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "wpscan", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "burpsuite", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "hashcat", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "gobuster", "-y"], check=True)
-        subprocess.run(["apt-get", "install", "dirb", "-y"], check=True)
-        subprocess.run(["apt", "install", "curl", "-y"], check=True)
-        subprocess.run(["apt", "install", "wordlists"], check=True)
-        subprocess.run(["gunzip", "/usr/share/wordlists/rockyou.txt.gz"], check=True)
-
-        print("Packages updated successfully.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    update_packages()
-EOF
+# Your system-level commands
+apt install -y wordlists
+gunzip /usr/share/wordlists/rockyou.txt.gz
