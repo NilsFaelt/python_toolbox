8fa7116e146f7798aa688b368f4464c4e52cb986
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-22 17:05:19 +0100

    test

---
+++ setup.py
@@ -0,0 +1,7 @@
+"""Setup file"""
+from setuptools import setup
+
+setup(
+    name='gemnina toolbox',
+    version='0.1'
+)
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Your system-level commands
+apt install -y wordlists
+gunzip /usr/share/wordlists/rockyou.txt.gz
