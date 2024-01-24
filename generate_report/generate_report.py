c883912dcace1bfafbb009097f90adf852a413a6
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-24 11:59:17 +0100

    added pdf generator

--- generate_report/generate_report.py
+++ generate_report/generate_report.py
@@ -1,5 +1,26 @@
-"""Generate report"""
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
 def generate_report():
-    """Function for generating report"""
-    title = input("Enter title for report: ")
-    print(f"Title {title}")
+    """Generate a pdf report"""
+    report_name = input("Enter name on report:")
+    title = input("Enter title: ")
+    content = input("Enter content: ")
+    report_name_fixed = report_name.strip() + ".pdf".strip()   
+    c = canvas.Canvas(report_name_fixed, pagesize=letter)
+
+    c.setFont("Helvetica", 16)
+    c.drawCentredString(300, 750, title)
+
+    c.setFont("Helvetica", 12)
+    text_object = c.beginText(100, 730)
+    text_object.setTextOrigin(100, 730)
+
+    for line in content.split('\n'):
+        text_object.textLine(line)
+
+    c.drawText(text_object)
+    c.save()
+
+if __name__ == "__main__":
+    generate_report()
