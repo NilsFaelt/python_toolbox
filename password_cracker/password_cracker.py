d204e0e29598756185bfcf5904666ed3dbc3e8e5
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-30 11:26:00 +0100

    rewind

--- password_cracker/password_cracker.py
+++ password_cracker/password_cracker.py
@@ -1,32 +1,32 @@
+"""function for cracking passwords"""
 import hashlib
+import logging
 
 file_path = '/usr/share/wordlists/rockyou.txt'
+hashed_words_array = []
 words_array = []
 
 
-def generate_hashed_words(file_path):
-    """Generator function to yield hashed words"""
+def password_cracker(encoded_password):
+    """function for cracking passwords"""
     try:
         with open(file_path, 'r', encoding='latin-1', errors='replace') as f:
             for line in f:
                 words = line.strip().split()
-                hashed_words = (hashlib.sha256(word.encode('utf-8')).hexdigest() for word in words)
+                hashed_words = [hashlib.sha256(word.encode('utf-8')).hexdigest() for word in words]
                 words_array.extend(words)
-                yield from hashed_words
+                hashed_words_array.extend(hashed_words)
+        compare_hashes(encoded_password, hashed_words_array)
     except Exception as e:
-        print(f"Error: {e}")
+           print(f"UniDecodeError: {e}")
 
-def password_cracker(encoded_password):
-    """Function for cracking passwords"""
-    hashed_words_generator = generate_hashed_words(file_path)
-    
-    for index, hashed_password in enumerate(hashed_words_generator):
+def compare_hashes(encoded_password, hashed_words_array):
+    """Check for matching hashes"""
+    for index, hashed_password in enumerate(hashed_words_array):
         if encoded_password == hashed_password:
             print(f"Match found at index {index}")
             print(f"Original word: {words_array[index]}")
             return
     print("No match found")
 
-if __name__ == "__main__":
-    encoded_password = input("Enter the encoded password to crack: ")
-    password_cracker(encoded_password)
+
